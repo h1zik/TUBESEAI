4b02b6e1a0d661a8b365a197b36f8648e5a1d183
h1zik <53518984+h1zik@users.noreply.github.com>
2024-05-18 16:00:17 +0700

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,14 +1,21 @@
+-- Create user_service_db and its tables
 CREATE DATABASE IF NOT EXISTS user_service_db;
-CREATE DATABASE IF NOT EXISTS topup_service_db;
-CREATE DATABASE IF NOT EXISTS main_app_db;
-
--- Switch to the new database
 USE user_service_db;
-
--- Create the users table if it doesn't exist
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     account_balance DECIMAL(10, 2) DEFAULT 0.00
 );
+
+-- Create topup_service_db and its tables
+CREATE DATABASE IF NOT EXISTS topup_service_db;
+USE topup_service_db;
+CREATE TABLE IF NOT EXISTS transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    amount DECIMAL(10, 2),
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE DATABASE IF NOT EXISTS main_app_db;
